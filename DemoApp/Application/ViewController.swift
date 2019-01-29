afdd0dd07fc0506033b6bf0efba56526ca02427d
Dariusz Rybicki <darrarski@gmail.com>
2019-01-29 22:27:10 +0100

    Add Xcode project

---
+++ DemoApp/Application/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        return nil
+    }
+
+    override func loadView() {
+        view = UIView(frame: .zero)
+        view.backgroundColor = .white
+    }
+
+}
