5898320bd1233eaa6676fb2e1873a5b86b2e6a6c
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 22:55:43 +0100

    Add name and price labels to ProductCardView

--- DemoApp/Deals/ProductCardView+Demo.swift
+++ DemoApp/Deals/ProductCardView+Demo.swift
@@ -5,18 +5,27 @@ extension ProductCardView {
         let view = ProductCardView()
         view.topBackgroundView.backgroundColor = .blackProduct
         view.imageView.image = .productOculus
+        view.updateImageViewLayout()
+        view.nameLabel.text = "Oculus Rift"
+        view.priceLabel.text = "$1 299"
         return view
     }
     static func surface() -> ProductCardView {
         let view = ProductCardView()
         view.topBackgroundView.backgroundColor = .blueProduct
         view.imageView.image = .productSurface
+        view.updateImageViewLayout()
+        view.nameLabel.text = "Surface Pro 4"
+        view.priceLabel.text = "$1 299"
         return view
     }
     static func xbox() -> ProductCardView {
         let view = ProductCardView()
         view.topBackgroundView.backgroundColor = .greenProduct
         view.imageView.image = .productXbox
+        view.updateImageViewLayout()
+        view.nameLabel.text = "xBox One"
+        view.priceLabel.text = "$699"
         return view
     }
 }
--- DemoApp/Deals/ProductCardView.swift
+++ DemoApp/Deals/ProductCardView.swift
@@ -26,28 +26,101 @@ class ProductCardView: UIView {
 
     let topBackgroundView = UIView(frame: .zero)
     let imageView = UIImageView(frame: .zero) |> UIImageView.productStyle
+    let nameLabel = UILabel(frame: .zero) |> UILabel.productNameStyle
+    let priceLabel = UILabel(frame: .zero) |> UILabel.priceStyle
+
+    private let topView = UIView(frame: .zero)
+    private let centerView = UIView(frame: .zero)
+    private let labelsStackView = UIStackView(arrangedSubviews: [])
+    private let bottomView = UIView(frame: .zero)
 
     private func setupSubviews() {
-        addSubview(topBackgroundView)
-        addSubview(imageView)
+        addSubview(topView)
+        topView.addSubview(topBackgroundView)
+        topView.addSubview(imageView)
+        addSubview(centerView)
+        centerView.addSubview(labelsStackView)
+        labelsStackView.addArrangedSubview(nameLabel)
+        labelsStackView.addArrangedSubview(priceLabel)
+        addSubview(bottomView)
     }
 
     // MARK: Layout
 
+    func updateImageViewLayout() {
+        guard let image = imageView.image, image.size.width > 0 else { return }
+        let aspectRatio = image.size.height / image.size.width
+        imageViewHeightConstraint?.isActive = false
+        imageViewHeightConstraint = imageView.heightAnchor.constraint(
+            equalTo: imageView.widthAnchor,
+            multiplier: aspectRatio
+        )
+        imageViewHeightConstraint?.isActive = true
+    }
+
+    private var imageViewHeightConstraint: NSLayoutConstraint?
+
     private func setupLayout() {
+        setupTopViewLayout()
+        setupCenterViewLayout()
+        setupBottomViewLayout()
+    }
+
+    private func setupTopViewLayout() {
+        topView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topView.topAnchor.constraint(equalTo: topAnchor),
+            topView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            topView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.5)
+        ])
+
         topBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            topBackgroundView.topAnchor.constraint(equalTo: topAnchor),
-            topBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            topBackgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            topBackgroundView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.39)
+            topBackgroundView.topAnchor.constraint(equalTo: topView.topAnchor),
+            topBackgroundView.leadingAnchor.constraint(equalTo: topView.leadingAnchor),
+            topBackgroundView.trailingAnchor.constraint(equalTo: topView.trailingAnchor),
+            topBackgroundView.heightAnchor.constraint(equalTo: topView.heightAnchor, multiplier: 0.77)
         ])
 
         imageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
-            imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
-            imageView.centerYAnchor.constraint(equalTo: topBackgroundView.bottomAnchor, constant: -10)
+            imageView.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 20),
+            imageView.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -20),
+            imageView.bottomAnchor.constraint(equalTo: topView.bottomAnchor)
+        ])
+    }
+
+    private func setupCenterViewLayout() {
+        centerView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            centerView.topAnchor.constraint(equalTo: topView.bottomAnchor),
+            centerView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            centerView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+
+        labelsStackView.axis = .vertical
+        labelsStackView.alignment = .center
+        labelsStackView.spacing = 8
+        labelsStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            labelsStackView.topAnchor.constraint(greaterThanOrEqualTo: centerView.topAnchor),
+            labelsStackView.leadingAnchor.constraint(greaterThanOrEqualTo: centerView.leadingAnchor, constant: 20),
+            labelsStackView.trailingAnchor.constraint(lessThanOrEqualTo: centerView.trailingAnchor, constant: -20),
+            labelsStackView.bottomAnchor.constraint(lessThanOrEqualTo: centerView.bottomAnchor),
+            labelsStackView.centerXAnchor.constraint(equalTo: centerView.centerXAnchor),
+            labelsStackView.centerYAnchor.constraint(equalTo: centerView.centerYAnchor)
+        ])
+    }
+
+    private func setupBottomViewLayout() {
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bottomView.topAnchor.constraint(equalTo: centerView.bottomAnchor),
+            bottomView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bottomView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            bottomView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            bottomView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.28)
         ])
     }
 
