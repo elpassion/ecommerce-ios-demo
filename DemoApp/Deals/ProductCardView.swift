9d430d52748ce9838ce2626b187d489d9682a086
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 14:56:30 +0100

    Add top background to ProductCardView

--- DemoApp/Deals/ProductCardView.swift
+++ DemoApp/Deals/ProductCardView.swift
@@ -5,11 +5,34 @@ class ProductCardView: UIView {
     init() {
         super.init(frame: .zero)
         backgroundColor = .white
+        clipsToBounds = true
         layer.cornerRadius = 8
+        setupSubviews()
+        setupLayout()
     }
 
     required init?(coder aDecoder: NSCoder) {
         return nil
+    }
+
+    // MARK: Subviews
+
+    let topBackgroundView = UIView(frame: .zero)
+
+    private func setupSubviews() {
+        addSubview(topBackgroundView)
+    }
+
+    // MARK: Layout
+
+    private func setupLayout() {
+        topBackgroundView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topBackgroundView.topAnchor.constraint(equalTo: topAnchor),
+            topBackgroundView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            topBackgroundView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            topBackgroundView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.39)
+        ])
     }
 
 }
