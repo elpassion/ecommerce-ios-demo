2b0a6653a73293d138b75a520cf715363023f4fe
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 14:15:05 +0100

    Add scrollable product views to DealsView

--- DemoApp/Deals/DealsView.swift
+++ DemoApp/Deals/DealsView.swift
@@ -13,11 +13,33 @@ class DealsView: UIView {
         return nil
     }
 
+    // MARK: Subviews
+
     let titleLabel = UILabel(frame: .zero) |> UILabel.titleStyle
+
+    var productViews: [UIView] = [] {
+        didSet {
+            oldValue.forEach { $0.removeFromSuperview() }
+            productViews.forEach {
+                stackView.addArrangedSubview($0)
+                setupLayout(productView: $0)
+            }
+        }
+    }
+
+    let scrollView = UIScrollView(frame: .zero)
+
+    private let stackView = UIStackView(arrangedSubviews: [])
 
     private func setupSubviews() {
         addSubview(titleLabel)
+        addSubview(scrollView)
+        scrollView.decelerationRate = .fast
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.addSubview(stackView)
     }
+
+    // MARK: Layout
 
     private func setupLayout() {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -26,6 +48,38 @@ class DealsView: UIView {
             titleLabel.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor),
             titleLabel.trailingAnchor.constraint(lessThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor),
             titleLabel.centerXAnchor.constraint(equalTo: safeAreaLayoutGuide.centerXAnchor)
+        ])
+
+        scrollView.contentInset = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 40)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 44),
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -78)
+        ])
+
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 24
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            stackView.heightAnchor.constraint(equalTo: scrollView.safeAreaLayoutGuide.heightAnchor)
+        ])
+    }
+
+    private func setupLayout(productView view: UIView) {
+        view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            view.widthAnchor.constraint(
+                equalTo: scrollView.safeAreaLayoutGuide.widthAnchor,
+                constant: -scrollView.adjustedContentInset.horizontal
+            )
         ])
     }
 
