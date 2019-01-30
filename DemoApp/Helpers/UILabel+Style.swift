9681c508876ff95cc96255125ea0470eec288fb8
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 22:47:17 +0100

    Add product name and price label styles

--- DemoApp/Helpers/UILabel+Style.swift
+++ DemoApp/Helpers/UILabel+Style.swift
@@ -6,4 +6,14 @@ extension UILabel {
         $0.adjustsFontForContentSizeCategory = true
         $0.textColor = .white
     }
+    static let productNameStyle: (UILabel) -> Void = {
+        $0.font = UIFontMetrics.default.scaledFont(for: .varelaRound(size: 32))
+        $0.adjustsFontForContentSizeCategory = true
+        $0.textColor = .darkText
+    }
+    static let priceStyle: (UILabel) -> Void = {
+        $0.font = UIFontMetrics.default.scaledFont(for: .varelaRound(size: 24))
+        $0.adjustsFontForContentSizeCategory = true
+        $0.textColor = UIColor.darkText.withAlphaComponent(0.5)
+    }
 }
