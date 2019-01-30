5a1b62f2a0cf5d84148f3d3968ae72f840e2b8ce
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 23:15:39 +0100

    Add SketchShadow

---
+++ DemoApp/Resources/SketchShadow+Assets.swift
@@ -0,0 +1,14 @@
+// swiftlint:disable object_literal
+import UIKit
+
+extension SketchShadow {
+    static var roundButtonShadow: SketchShadow {
+        return SketchShadow(
+            color: UIColor(red: 0.973, green: 0.259, blue: 0.227, alpha: 0.4),
+            x: 0,
+            y: 8,
+            blur: 22,
+            spread: 0
+        )
+    }
+}
