10e31562604318b8fc72b9c629f3332cce3aee66
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 13:34:17 +0100

    Update UILabel+Style extension

--- DemoApp/Helpers/UILabel+Style.swift
+++ DemoApp/Helpers/UILabel+Style.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 extension UILabel {
-    static var titleStyle: (UILabel) -> Void = {
+    static let titleStyle: (UILabel) -> Void = {
         $0.font = UIFontMetrics.default.scaledFont(for: .varelaRound(size: 20))
         $0.adjustsFontForContentSizeCategory = true
         $0.textColor = .white
