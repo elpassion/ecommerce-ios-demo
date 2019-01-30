21b9c9ee8f44fbfd46a6840382a7060a021d5347
Dariusz Rybicki <darrarski@gmail.com>
2019-01-30 22:56:18 +0100

    Add GradientView

---
+++ DemoApp/Resources/GradientStyle+Assets.swift
@@ -0,0 +1,14 @@
+// swiftlint:disable object_literal
+import UIKit
+
+extension GradientStyle {
+    static var button: GradientStyle {
+        return GradientStyle(
+            colors: [
+                GradientColor(location: 0, color: UIColor(red: 0.933, green: 0.035, blue: 0.475, alpha: 1)),
+                GradientColor(location: 1, color: UIColor(red: 1, green: 0.416, blue: 0, alpha: 1))
+            ],
+            direction: .horizontal
+        )
+    }
+}
