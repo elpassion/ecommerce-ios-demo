34dba875c8de31e5a5cf5a2b36cac965979eacc6
Dariusz Rybicki <darrarski@gmail.com>
2019-01-31 23:12:05 +0100

    Integrate Quick and Nimble via Carthage

---
+++ DemoAppTests/DemoAppSpec.swift
@@ -0,0 +1,13 @@
+import Quick
+import Nimble
+@testable import DemoApp
+
+class DemoAppSpec: QuickSpec {
+    override func spec() {
+        describe("test") {
+            it("should pass") {
+                expect(true) == true
+            }
+        }
+    }
+}
