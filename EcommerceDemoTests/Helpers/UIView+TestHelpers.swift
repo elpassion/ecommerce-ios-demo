ec72a3f786cb117a40a2fdb74959dd9bec8cbb30
Dariusz Rybicki <darrarski@gmail.com>
2019-02-01 14:19:07 +0100

    Add UIView+Helpers tests helpers extesnion

---
+++ EcommerceDemoTests/Helpers/UIView+TestHelpers.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+extension UIView {
+    func constrained(size: CGSize) -> UIView {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            widthAnchor.constraint(equalToConstant: size.width),
+            heightAnchor.constraint(equalToConstant: size.height)
+        ])
+        return self
+    }
+}
+
+func aView(with sut: UIView) -> UIView {
+    let view = UIView(frame: .zero)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(sut)
+    sut.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        sut.topAnchor.constraint(equalTo: view.topAnchor),
+        sut.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        sut.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        sut.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+    ])
+    view.setNeedsLayout()
+    view.layoutIfNeeded()
+    return view
+}
