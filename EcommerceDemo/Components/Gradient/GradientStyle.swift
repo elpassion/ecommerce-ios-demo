bba5d6ee10ee19a6d43328c86bd8c66fb673ba1f
Dariusz Rybicki <darrarski@gmail.com>
2019-02-01 19:11:58 +0100

    Organize files

---
+++ EcommerceDemo/Components/Gradient/GradientStyle.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+struct GradientStyle {
+    let colors: [GradientColor]
+    let direction: GradientDirection
+}
+
+struct GradientColor {
+    let location: Float
+    let color: UIColor
+}
+
+struct GradientDirection {
+    let startPoint: CGPoint
+    let endPoint: CGPoint
+}
+
+extension GradientDirection {
+    static var horizontal: GradientDirection {
+        return GradientDirection(
+            startPoint: CGPoint(x: 0, y: 0.5),
+            endPoint: CGPoint(x: 1, y: 0.5)
+        )
+    }
+    static var vertical: GradientDirection {
+        return GradientDirection(
+            startPoint: CGPoint(x: 0.5, y: 0),
+            endPoint: CGPoint(x: 0.5, y: 1)
+        )
+    }
+}
--- EcommerceDemo/Components/Gradient/GradientView.swift
+++ EcommerceDemo/Components/Gradient/GradientView.swift
@@ -24,33 +24,3 @@ class GradientView: UIView {
     private let gradientLayer = CAGradientLayer()
 
 }
-
-struct GradientStyle {
-    let colors: [GradientColor]
-    let direction: GradientDirection
-}
-
-struct GradientColor {
-    let location: Float
-    let color: UIColor
-}
-
-struct GradientDirection {
-    let startPoint: CGPoint
-    let endPoint: CGPoint
-}
-
-extension GradientDirection {
-    static var horizontal: GradientDirection {
-        return GradientDirection(
-            startPoint: CGPoint(x: 0, y: 0.5),
-            endPoint: CGPoint(x: 1, y: 0.5)
-        )
-    }
-    static var vertical: GradientDirection {
-        return GradientDirection(
-            startPoint: CGPoint(x: 0.5, y: 0),
-            endPoint: CGPoint(x: 0.5, y: 1)
-        )
-    }
-}
