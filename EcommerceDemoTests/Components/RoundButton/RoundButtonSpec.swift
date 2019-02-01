687a4bbb82bb9b68783199b0c6eb3d7a1431c33d
Dariusz Rybicki <darrarski@gmail.com>
2019-02-01 19:29:10 +0100

    Add RoundButtonSpec

---
+++ EcommerceDemoTests/Components/RoundButton/RoundButtonSpec.swift
@@ -0,0 +1,27 @@
+import Quick
+import Nimble
+import SnapshotTesting
+@testable import EcommerceDemo
+
+class RoundButtonSpec: QuickSpec {
+    override func spec() {
+        describe("RoundButton") {
+            var sut: RoundButton!
+
+            beforeEach {
+                sut = RoundButton()
+                sut.titleLabel.text = "Button Title"
+            }
+
+            it("should have correct snapshot") {
+                assertSnapshot(matching: aView(with: sut, margin: 100), as: .image)
+            }
+        }
+
+        describe("with coder") {
+            it("should be nil") {
+                expect(RoundButton(coder: NSCoder())).to(beNil())
+            }
+        }
+    }
+}
--- EcommerceDemoTests/Helpers/UIView+TestHelpers.swift
+++ EcommerceDemoTests/Helpers/UIView+TestHelpers.swift
@@ -11,16 +11,16 @@ extension UIView {
     }
 }
 
-func aView(with sut: UIView) -> UIView {
+func aView(with sut: UIView, margin: CGFloat = 0) -> UIView {
     let view = UIView(frame: .zero)
     view.translatesAutoresizingMaskIntoConstraints = false
     view.addSubview(sut)
     sut.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-        sut.topAnchor.constraint(equalTo: view.topAnchor),
-        sut.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-        sut.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        sut.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        sut.topAnchor.constraint(equalTo: view.topAnchor, constant: margin),
+        sut.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: margin),
+        sut.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -margin),
+        sut.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -margin)
     ])
     view.setNeedsLayout()
     view.layoutIfNeeded()
