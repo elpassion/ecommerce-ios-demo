976a1957058d5edbf1e36dc32f580d798b6c4385
Dariusz Rybicki <darrarski@gmail.com>
2019-02-01 19:24:53 +0100

    Add test to ProductCardViewSpec

--- EcommerceDemoTests/Components/ProductCard/ProductCardViewSpec.swift
+++ EcommerceDemoTests/Components/ProductCard/ProductCardViewSpec.swift
@@ -1,9 +1,28 @@
 import Quick
 import Nimble
+import SnapshotTesting
 @testable import EcommerceDemo
 
 class ProductCardViewSpec: QuickSpec {
     override func spec() {
+        context("init") {
+            var sut: ProductCardView!
+
+            beforeEach {
+                sut = ProductCardView()
+            }
+
+            context("update image view layout") {
+                beforeEach {
+                    sut.updateImageViewLayout()
+                }
+
+                it("should have correct snapshot") {
+                    assertSnapshot(matching: aView(with: sut), as: .image, named: "empty")
+                }
+            }
+        }
+
         describe("with coder") {
             it("should be nil") {
                 expect(ProductCardView(coder: NSCoder())).to(beNil())
