3acffe69387db112d313f8ddb94f35cfea512a51
Dariusz Rybicki <darrarski@gmail.com>
2019-02-01 17:49:10 +0100

    Rotate product images on scroll

--- EcommerceDemo/Components/ProductCard/ProductCardViewController.swift
+++ EcommerceDemo/Components/ProductCard/ProductCardViewController.swift
@@ -39,7 +39,10 @@ class ProductCardViewController: UIViewController {
         super.viewDidLayoutSubviews()
         let progress = min(1, max(-1, distanceFromCenter / view.bounds.width))
         productCardView.topBackgroundView.alpha = 1 - abs(progress * 0.25)
-        productCardView.imageView.transform = CGAffineTransform(translationX: 0, y: -12 * abs(progress))
+        var imageViewTransform = CGAffineTransform.identity
+        imageViewTransform = imageViewTransform.translatedBy(x: 32 * progress, y: 0)
+        imageViewTransform = imageViewTransform.rotated(by: -9 / 360 * CGFloat.pi * progress)
+        productCardView.imageView.transform = imageViewTransform
     }
 
     // MARK: Private
