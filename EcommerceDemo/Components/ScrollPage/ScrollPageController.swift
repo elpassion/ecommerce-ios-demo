c2c86a47c719a66c23338037c101c5d86bfe97b3
Dariusz Rybicki <darrarski@gmail.com>
2019-02-04 21:08:16 +0100

    Revert "Refactor ScrollPageController"
    This reverts commit ce1c4103f0b38da4541e07cbd1571977502c3d1c.

--- EcommerceDemo/Components/ScrollPage/ScrollPageController.swift
+++ EcommerceDemo/Components/ScrollPage/ScrollPageController.swift
@@ -1,48 +1,21 @@
 import UIKit
 
 struct ScrollPageController {
-
-    typealias Page = (index: Int, offset: CGFloat)
-
-    func page(for offset: CGFloat, velocity: CGFloat, in pageOffsets: [CGFloat]) -> Page? {
-        guard let currentPage = currentPage(for: offset, in: pageOffsets) else {
+    func pageOffset(for offset: CGFloat, velocity: CGFloat, in pageOffsets: [CGFloat]) -> CGFloat? {
+        let pages = pageOffsets.enumerated().reduce([Int: CGFloat]()) {
+            var dict = $0
+            dict[$1.0] = $1.1
+            return dict
+        }
+        guard let page = pages.min(by: { abs($0.1 - offset) < abs($1.1 - offset) }) else {
             return nil
         }
         if abs(velocity) < 0.2 {
-            return currentPage
+            return page.value
         }
-        if velocity > 0 {
-            return nextPage(for: currentPage, in: pageOffsets) ?? currentPage
+        if velocity < 0 {
+            return pages[pageOffsets.index(before: page.key)] ?? page.value
         }
-        return previousPage(for: currentPage, in: pageOffsets) ?? currentPage
+        return pages[pageOffsets.index(after: page.key)] ?? page.value
     }
-
-    private func currentPage(for offset: CGFloat, in pageOffsets: [CGFloat]) -> Page? {
-        let pageOffsetsDict: [Int: CGFloat] = pageOffsets.enumerated().reduce(.init()) {
-            var dict = $0
-            dict[$1.offset] = $1.element
-            return dict
-        }
-        guard let page = pageOffsetsDict.min(by: { abs($0.value - offset) < abs($1.value - offset) }) else {
-            return nil
-        }
-        return (index: page.key, offset: page.value)
-    }
-
-    private func previousPage(for page: Page, in pageOffsets: [CGFloat]) -> Page? {
-        let pageIndex = pageOffsets.index(before: page.index)
-        guard (pageOffsets.startIndex..<pageOffsets.endIndex).contains(pageIndex) else {
-            return nil
-        }
-        return (index: pageIndex, offset: pageOffsets[pageIndex])
-    }
-
-    private func nextPage(for page: Page, in pageOffsets: [CGFloat]) -> Page? {
-        let pageIndex = pageOffsets.index(after: page.index)
-        guard (pageOffsets.startIndex..<pageOffsets.endIndex).contains(pageIndex) else {
-            return nil
-        }
-        return (index: pageIndex, offset: pageOffsets[pageIndex])
-    }
-
 }
