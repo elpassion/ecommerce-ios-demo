a07472e3ce8572156b9d95a8f5d07c3c578e118d
Dariusz Rybicki <darrarski@gmail.com>
2019-02-05 21:57:15 +0100

    Add Product.Spec model

--- EcommerceDemo/Models/Product.swift
+++ EcommerceDemo/Models/Product.swift
@@ -1,10 +1,17 @@
 import UIKit
 
 struct Product {
+    struct Spec {
+        let name: String
+        let value: String
+        let icon: UIImage
+    }
+
     let name: String
     let price: String
     let color: UIColor
     let image: UIImage
+    let specs: [Spec]
 }
 
 extension Product {
@@ -12,18 +19,33 @@ extension Product {
         name: "Oculus Rift",
         price: "$1 299",
         color: .blackProduct,
-        image: .productOculus
+        image: .productOculus,
+        specs: [
+            Spec(name: "diagonal", value: "12.3\"", icon: .iconDiagonal),
+            Spec(name: "hard drive", value: "1TB", icon: .iconHardDrive),
+            Spec(name: "RAM", value: "16GB", icon: .iconRam)
+        ]
     )
     static let surface = Product(
         name: "Surface Pro 4",
         price: "$1 299",
         color: .blueProduct,
-        image: .productSurface
+        image: .productSurface,
+        specs: [
+            Spec(name: "diagonal", value: "12.3\"", icon: .iconDiagonal),
+            Spec(name: "hard drive", value: "1TB", icon: .iconHardDrive),
+            Spec(name: "RAM", value: "16GB", icon: .iconRam)
+        ]
     )
     static let xbox = Product(
         name: "xBox One",
         price: "$699",
         color: .greenProduct,
-        image: .productXbox
+        image: .productXbox,
+        specs: [
+            Spec(name: "diagonal", value: "12.3\"", icon: .iconDiagonal),
+            Spec(name: "hard drive", value: "1TB", icon: .iconHardDrive),
+            Spec(name: "RAM", value: "16GB", icon: .iconRam)
+        ]
     )
 }
