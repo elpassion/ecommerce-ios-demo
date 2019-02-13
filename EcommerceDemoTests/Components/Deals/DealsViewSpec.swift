7ebbfc2fc7a30cfbc14f424dae47e66a0d399952
Dariusz Rybicki <darrarski@gmail.com>
2019-02-13 03:40:36 +0100

    Add tests to DealsViewSpec

--- EcommerceDemoTests/Components/Deals/DealsViewSpec.swift
+++ EcommerceDemoTests/Components/Deals/DealsViewSpec.swift
@@ -4,6 +4,37 @@ import Nimble
 
 class DealsViewSpec: QuickSpec {
     override func spec() {
+        describe("init") {
+            var sut: DealsView!
+
+            beforeEach {
+                sut = DealsView()
+            }
+
+            context("add product views") {
+                var productViews: [UIView]!
+
+                beforeEach {
+                    productViews = [UIView(), UIView(), UIView()]
+                    sut.productViews = productViews
+                }
+
+                it("should add product views to view hierarchy") {
+                    expect(productViews.compactMap { $0.superview }).to(haveCount(productViews.count))
+                }
+
+                context("remove product views") {
+                    beforeEach {
+                        sut.productViews = []
+                    }
+
+                    it("should remove old product views from view hierarchy") {
+                        expect(productViews.compactMap { $0.superview }).to(beEmpty())
+                    }
+                }
+            }
+        }
+
         describe("with coder") {
             it("should be nil") {
                 expect(DealsView(coder: NSCoder())).to(beNil())
